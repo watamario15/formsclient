d92d89a303c07e855482191fc794b13580d49948
watamario15 <watamario15@users.noreply.github.com>
2022-07-14 15:52:21 +0000

    deploy: 6ab9fa0b3b0024a57ab4112704474445e73cabb4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"index.html": "64dfa5167206fde5065ca5f9866568b8",
-"/": "64dfa5167206fde5065ca5f9866568b8",
+"index.html": "35f9bb6164633777b4381ff60a092e86",
+"/": "35f9bb6164633777b4381ff60a092e86",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "54d40abf934323b988adbaa33522d4fd"
 };
