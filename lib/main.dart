17034230763b660bd64d16ac53df918874aa85dd
watamario15 <watamario15@outlook.jp>
2022-07-18 00:44:46 +0900

    Fetch from a server

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'dart:async';
 
 class Selection {
@@ -28,7 +28,9 @@ class Question {
       : id = json['id']!,
         type = json['type']!,
         explanation = json['explanation']!,
-        selections = [for (var item in json['selection']!) item];
+        selections = [
+          for (var item in json['selection']!) Selection.fromJson(item)
+        ];
 
   Map<String, dynamic> toJson() => {
         'id': id,
@@ -39,16 +41,21 @@ class Question {
 }
 
 class MyFormModel {
+  final String id;
   final String title;
   final List<Question> questions;
 
-  const MyFormModel(this.title, this.questions);
+  const MyFormModel(this.id, this.title, this.questions);
 
   MyFormModel.fromJson(Map<String, dynamic> json)
-      : title = json['title']!,
-        questions = [for (var item in json['questions']!) item];
+      : id = json['id']!,
+        title = json['title']!,
+        questions = [
+          for (var item in json['questions']!) Question.fromJson(item)
+        ];
 
-  Map<String, dynamic> toJson() => {'title': title, 'questions': questions};
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'title': title, 'questions': questions};
 }
 
 class GetFormModel {
@@ -61,29 +68,71 @@ class GetFormModel {
   GetFormModel.fromJson(Map<String, dynamic> json)
       : result = json['result']!,
         id = json['id']!,
-        form = json['form']!;
+        form = MyFormModel.fromJson(json['form']!);
 
   Map<String, dynamic> toJson() => {'result': result, 'id': id, 'form': form};
 }
 
-class TextAnswer {
+class Answer {
   final int id;
   final String type;
-  final String value;
-}
+  String? textValue;
+  Map<int, bool>? radioValue;
 
-class RadioAnswer {
-  final int id;
-  final String type;
-  
+  Answer(this.id, this.type, dynamic value) {
+    if (type == 'text') {
+      textValue = value;
+    } else if (type == 'radio') {
+      radioValue = value;
+    } else {
+      throw Exception("No such type: $type.");
+    }
+  }
+
+  Answer.fromJson(Map<String, dynamic> json)
+      : id = json['id']!,
+        type = json['type']! {
+    if (type == 'text') {
+      textValue = json['value']!;
+    } else if (type == 'radio') {
+      radioValue = json['value']!;
+    } else {
+      throw Exception("No such type: $type.");
+    }
+  }
+
+  Map<String, dynamic> toJson() =>
+      {'id': id, 'type': type, 'value': textValue ?? radioValue};
 }
 
 class AnswerModel {
   final String id;
   final List<Answer> answers;
+
+  const AnswerModel(this.id, this.answers);
+
+  AnswerModel.fromJson(Map<String, dynamic> json)
+      : id = json['id']!,
+        answers = [for (var item in json['answers']!) Answer.fromJson(item)];
+
+  Map<String, dynamic> toJson() => {'id': id, 'answers': answers};
 }
 
+class AnswerResponse {
+  final String result;
+  final String? id;
+  final String message;
 
+  const AnswerResponse(this.result, this.id, this.message);
+
+  AnswerResponse.fromJson(Map<String, dynamic> json)
+      : result = json['result']!,
+        id = json['id'],
+        message = json['message']!;
+
+  Map<String, dynamic> toJson() =>
+      {'result': result, if (id != null) 'id': id, 'message': message};
+}
 
 class CheckboxFormField extends FormField<Map<int, bool>> {
   CheckboxFormField({
@@ -121,6 +170,8 @@ class CheckboxFormField extends FormField<Map<int, bool>> {
         );
 }
 
+String? hostName, formID;
+
 class MyForm extends StatefulWidget {
   const MyForm({Key? key}) : super(key: key);
 
@@ -130,29 +181,8 @@ class MyForm extends StatefulWidget {
 
 class _MyFormState extends State<MyForm> {
   final _formKey = GlobalKey<FormState>();
-  late Future<GetFormModel> futureForm;
-  Future<GetFormModel> fetchForm(String id) async {
-    final response =
-        await http.get(Uri.parse('http://localhost:8080/get-form/$id'));
+  Future<GetFormModel>? _futureForm;
 
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      return GetFormModel.fromJson(jsonDecode(response.body));
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    futureForm = fetchForm("0");
-  }
-
-  // final _json = JSONForm.fromJson(jsonDecode(jsonString));
   /*
   final _formResponse = const GetFormModel(
     'ok',
@@ -176,67 +206,103 @@ class _MyFormState extends State<MyForm> {
   );
   */
 
+  Future<GetFormModel> fetchForm(String host, String id) async {
+    final response =
+        await http.get(Uri.parse('http://$host:8080/get-form/$id'));
+
+    if (response.statusCode == 200) {
+      return GetFormModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to fetch form "$id" from $host.');
+    }
+  }
+
   /// タイトル
-  Widget _title(String title) => Container(
-        decoration: const BoxDecoration(
-          color: Colors.black26,
-        ),
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                title,
-                style: Theme.of(context).textTheme.headline3,
+  Widget _title(String title) => Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  title,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 255, 0, 0),
-                    onPrimary: const Color.fromARGB(255, 0, 0, 0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.resolveWith(
+                          (states) => states.contains(MaterialState.disabled)
+                              ? null
+                              : Colors.white),
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                          (states) => states.contains(MaterialState.disabled)
+                              ? null
+                              : Colors.blue),
+                    ),
+                    onPressed: null, // 未実装
+                    child: const Text('Modify this form'),
                   ),
-                  onPressed: null, // 未実装
-                  child: const Text('編集'),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: const Color.fromARGB(255, 255, 0, 0),
-                    onPrimary: const Color.fromARGB(255, 0, 0, 0),
+                  TextButton(
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.resolveWith(
+                          (states) => states.contains(MaterialState.disabled)
+                              ? null
+                              : Colors.white),
+                      backgroundColor: MaterialStateProperty.resolveWith(
+                          (states) => states.contains(MaterialState.disabled)
+                              ? null
+                              : Colors.blue),
+                    ),
+                    onPressed: null, // 未実装
+                    child: const Text('Show answers'),
                   ),
-                  onPressed: null, // 未実装
-                  child: const Text('集計'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       );
 
   /// 質問項目
-  Widget _formItem(Question question) => Container(
-        decoration: const BoxDecoration(
-          color: Colors.black26,
-        ),
-        child: Column(
-          children: [
-            Text(
-              question.explanation,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            question.type == 'text'
-                ? TextFormField(
-                    onSaved: (content) {},
-                  )
-                : CheckboxFormField(
-                    options: question.selections,
-                    onSaved: (selected) {},
-                  ),
-          ],
+  Widget _formItem(Question question) => Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  question.explanation,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+              ),
+              question.type == 'text'
+                  ? TextFormField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      onSaved: (content) {},
+                    )
+                  : CheckboxFormField(
+                      options: question.selections,
+                      onSaved: (selected) {},
+                    ),
+            ],
+          ),
         ),
       );
+
+  @override
+  void initState() {
+    super.initState();
+    _futureForm = fetchForm(hostName!, formID!);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -245,30 +311,145 @@ class _MyFormState extends State<MyForm> {
         centerTitle: true,
         title: const Text('Zemi-A Forms'),
       ),
-      body: Form(
-        key: _formKey,
-        child: FutureBuilder<GetFormModel>(
-          future: futureForm,
-          builder: (context, snapshot) {
-            if(snapshot.hasData){
-              return ListView(
+      body: FutureBuilder<GetFormModel>(
+        future: _futureForm,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Form(
+              key: _formKey,
+              child: ListView(
                 children: [
                   _title(snapshot.data!.form.title),
-                  for (var item in snapshot.data!.form.questions) _formItem(item),
+                  for (var item in snapshot.data!.form.questions)
+                    _formItem(item),
                 ],
-              );
-            } else if (snapshot.hasError){
-              return Text('${snapshot.error}');
-            }
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: SizedBox(
+                width: 400,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Failed to fetch a form. '
+                      'Please make sure you entered a server and an ID correctly.\n\n'
+                      'Error: ${snapshot.error}',
+                      style: Theme.of(context).textTheme.bodyLarge,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
 
-            return const CircularProgressIndicator();
-          },
-        ),
+          return const Center(child: CircularProgressIndicator());
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _formKey.currentState!.save(),
-        tooltip: '送信',
+        tooltip: 'Submit',
         child: const Icon(Icons.send),
+      ),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Sign in',
+                        style: Theme.of(context).textTheme.headline4),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration:
+                          const InputDecoration(hintText: 'Form server'),
+                      onSaved: (value) {
+                        hostName = value;
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a form server name.';
+                        }
+                        return null;
+                      },
+                      autofocus: true,
+                      textInputAction: TextInputAction.next,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(hintText: 'Form ID'),
+                      onSaved: (value) {
+                        formID = value;
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter an form ID.';
+                        }
+                        return null;
+                      },
+                      onFieldSubmitted: (value) {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.of(context).pushNamed('/answer');
+                        }
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextButton(
+                      style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.resolveWith(
+                            (states) => states.contains(MaterialState.disabled)
+                                ? null
+                                : Colors.white),
+                        backgroundColor: MaterialStateProperty.resolveWith(
+                            (states) => states.contains(MaterialState.disabled)
+                                ? null
+                                : Colors.blue),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          Navigator.of(context).pushNamed('/answer');
+                        }
+                      },
+                      child: const Text('Sign in'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -289,7 +470,10 @@ class MyFormApp extends StatelessWidget {
         brightness: Brightness.dark,
         useMaterial3: true,
       ),
-      home: const MyForm(),
+      routes: {
+        '/': (context) => const Home(),
+        '/answer': (context) => const MyForm(),
+      },
     );
   }
 }
